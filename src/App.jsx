49f8da62ab2baeb136da0b7b4a34d5e181fe3738
singwithaashish <newsingaasi@outlook.com>
2023-08-28 02:48:12 +0530

    update graph to single page, add option to compare and reset

--- src/App.jsx
+++ src/App.jsx
@@ -56,62 +56,23 @@ function App() {
       // if((selectedValues.firstGraph.endIndex - selectedValues.firstGraph.startIndex) > (selectedValues.secondGraph.endIndex - selectedValues.secondGraph.startIndex)){
 
       if (graph === 1) {
-        const index = getElementAtEvent(chartRef.current, e)[0].index;
-       if(index <= selectedValues.firstGraph.startIndex){
-          setSelectedValues({
-            firstGraph: {
-              ...selectedValues.firstGraph,
-              startIndex: firstGraph[index].x,
-            },
-            secondGraph: {
-              ...selectedValues.secondGraph,
-              startIndex: secondGraph[index].x,
-            },
-          });
-        }
-        // if firstLength - index < secondLength - secondIndex then secondGraph's end should be secondIndex + (firstLength - index)
-        if (
-          (selectedValues.firstGraph.endIndex - index) <
-          (selectedValues.secondGraph.endIndex -
-            selectedValues.secondGraph.startIndex)
-        ) {
-          setSelectedValues({
-            firstGraph: {
-              ...selectedValues.firstGraph,
-              startIndex: firstGraph[index].x,
-            },
-            secondGraph: {
-              ...selectedValues.secondGraph,
-              endIndex: secondGraph[selectedValues.secondGraph.startIndex + (selectedValues.firstGraph.endIndex - index)].x,
-            },
-          });
-        } else {
-          setSelectedValues({
-            ...selectedValues,
-            firstGraph: {
-              ...selectedValues.firstGraph,
-              startIndex: firstGraph[index].x,
-            },
-          });
-        }
+        const index = getElementAtEvent(chartRef.current, e)[0].index; // start index
+        setSelectedValues({
+          ...selectedValues,
+          firstGraph: {
+            ...selectedValues.firstGraph,
+            startIndex: firstGraph[index].x,
+          },
+        });
       } else {
-        const index = getElementAtEvent(chatRef2.current, e)[0].index;
-        if (
-          (selectedValues.secondGraph.endIndex - index) <
-          selectedValues.firstGraph.endIndex -
-            selectedValues.firstGraph.startIndex
-        ) {
-          setSelectedValues({
-            secondGraph: {
-              ...selectedValues.secondGraph,
-              startIndex: secondGraph[index].x,
-            },
-            firstGraph: {
-              ...selectedValues.firstGraph,
-              endIndex: firstGraph[selectedValues.firstGraph.startIndex + (selectedValues.secondGraph.endIndex - index)].x,
-            },
-          });
-        }
+        const index = getElementAtEvent(chatRef2.current, e)[0].index; // start index
+        setSelectedValues({
+          ...selectedValues,
+          secondGraph: {
+            ...selectedValues.secondGraph,
+            startIndex: secondGraph[index].x,
+          },
+        });
       }
     } catch (e) {
       console.log(e);
@@ -119,76 +80,74 @@ function App() {
   };
 
   const onClickPoint = (e) => {
-    try {
-      setFirstGraph(firstGraph.splice(selectedValues.firstGraph.startIndex, selectedValues.firstGraph.endIndex));
-      setSecondGraph(secondGraph.splice(selectedValues.secondGraph.startIndex, selectedValues.secondGraph.endIndex));
-    //   // when user clicks, set the index as the first index of the graph
-    //   if (graph === 1) {
-    //     const index = getElementAtEvent(chartRef.current, e)[0].index;
-
-    //     // if lastINdex - index < lastIndexofSecondGraph - indexOfSecondGraph then second Grap's end should be indexOfSecondGraph + (lastIndex - index)
-    //     if (
-    //       secondGraph.length - index <
-    //       selectedValues.secondGraph.endIndex -
-    //         selectedValues.secondGraph.startIndex
-    //     ) {
-    //       setSecondGraph(secondGraph.splice(index, secondGraph.length - index));
-    //       setFirstGraph(firstGraph.splice(index, firstGraph.length - index));
-    //     }
-    //     // else firstGraph's end should be indexOfSecondGraph + (lastIndex - index)
-    //     else {
-    //       setSecondGraph(
-    //         secondGraph.splice(
-    //           index,
-    //           selectedValues.secondGraph.endIndex -
-    //             selectedValues.secondGraph.startIndex
-    //         )
-    //       );
-    //       setFirstGraph(
-    //         firstGraph.splice(
-    //           index,
-    //           selectedValues.secondGraph.endIndex -
-    //             selectedValues.secondGraph.startIndex
-    //         )
-    //       );
-    //     }
-    //     // setFirstGraph(firstGraph.splice(index, firstGraph.length - index));
-    //     // setSecondGraph(secondGraph.splice(index, secondGraph.length - index));
-    //   } else {
-    //     const index = getElementAtEvent(chatRef2.current, e)[0].index;
-
-    //     if (
-    //       firstGraph.length - index <
-    //       selectedValues.firstGraph.endIndex -
-    //         selectedValues.firstGraph.startIndex
-    //     ) {
-    //       setSecondGraph(secondGraph.splice(index, secondGraph.length - index));
-    //       setFirstGraph(firstGraph.splice(index, firstGraph.length - index));
-    //     } else {
-    //       setSecondGraph(
-    //         secondGraph.splice(
-    //           index,
-    //           selectedValues.firstGraph.endIndex -
-    //             selectedValues.firstGraph.startIndex
-    //         )
-    //       );
-    //       setFirstGraph(
-    //         firstGraph.splice(
-    //           index,
-    //           selectedValues.firstGraph.endIndex -
-    //             selectedValues.firstGraph.startIndex
-    //         )
-    //       );
-    //     }
-
-    //     // setSecondGraph(secondGraph.splice(index, secondGraph.length - index));
-    //     // setFirstGraph(firstGraph.splice(index, firstGraph.length - index));
-    //   }
-    } catch (e) {
-      console.log(e);
-    }
+   
+      if (
+        selectedValues.secondGraph.endIndex -
+          selectedValues.secondGraph.startIndex <
+        selectedValues.firstGraph.endIndex -
+          selectedValues.firstGraph.startIndex
+      ) {
+        setSelectedValues({
+          ...selectedValues,
+          firstGraph: {
+            ...selectedValues.firstGraph,
+            endIndex:
+              selectedValues.firstGraph.startIndex +
+              (selectedValues.secondGraph.endIndex -
+                selectedValues.secondGraph.startIndex),
+          },
+        });
+      } else {
+        setSelectedValues({
+          ...selectedValues,
+          secondGraph: {
+            ...selectedValues.secondGraph,
+            endIndex:
+              selectedValues.secondGraph.startIndex +
+              (selectedValues.firstGraph.endIndex -
+                selectedValues.firstGraph.startIndex),
+          },
+        });
+      }
+      setFirstGraph(
+        firstGraph.slice(
+          firstGraph.findIndex(
+            (item) => item.x === selectedValues.firstGraph.startIndex
+          ),
+          firstGraph.findIndex(
+            (item) => item.x === selectedValues.firstGraph.endIndex
+          )
+        )
+      );
+      setSecondGraph(
+        secondGraph.slice(
+          secondGraph.findIndex(
+            (item) => item.x === selectedValues.secondGraph.startIndex
+          ),
+          secondGraph.findIndex(
+            (item) => item.x === selectedValues.secondGraph.endIndex
+          )
+        )
+      );
+    
     // console.log(getElementAtEvent(chartRef.current, e));
   };
+
+
+  const reset = () => {
+    setFirstGraph(graph1);
+    setSecondGraph(graph2);
+    setSelectedValues({
+      firstGraph: {
+        startIndex: 0,
+        endIndex: firstGraph.length - 1,
+      },
+      secondGraph: {
+        startIndex: 0,
+        endIndex: secondGraph.length - 1,
+      },
+    });
+  }
 
   return (
     <div
@@ -196,91 +155,107 @@ function App() {
         minHeight: "100vh",
         // display: 'flex',
         color: "black",
-        backgroundColor: "white",
+        backgroundColor: "white",//"rgb(229, 231, 235)",
         alignItems: "center",
         width: "100vw",
       }}
     >
       <div
         style={{
+          position: "absolute",
+          top: "10px",
+          right: "10px",
+          zIndex: "100",
+          color: "black",
           display: "flex",
-          justifyContent: "space-evenly",
-          width: "100%",
+          flexDirection: "column",
+          alignItems: "center",
+          // backdropFilter: 'blur(10px)',
+          backdropFilter: "blur(3px)",
+          padding: "10px",
+          borderRadius: "10px",
+          backgroundColor: "rgb(229, 231, 235)",
         }}
       >
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <h1>Graph 1</h1>
-          <div>
-            <h3>Start Index: {selectedValues.firstGraph.startIndex}</h3>
-            <h3>End Index: {selectedValues.firstGraph.endIndex}</h3>
-          </div>
+        <h3>
+          A ({selectedValues.firstGraph.startIndex},{" "}
+          {selectedValues.firstGraph.endIndex})
+        </h3>
+        <h3>
+          B ({selectedValues.secondGraph.startIndex},{" "}
+          {selectedValues.secondGraph.endIndex})
+        </h3>
+
+        <button onClick={onClickPoint} style={{
+          marginBottom: '10px'
+        }}>Update</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <div
+        style={{
+          height: "100vh",
+          width: "100vw",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          
+        }}
+      >
+        <div style={{ height: "50%", width: "99%", display: "flex", justifyContent: "center" }}>
+          <Line
+            data={{
+              labels: firstGraph.map((item) => item.x),
+              datasets: [
+                {
+                  label: "Y value",
+                  data: firstGraph.map((item) => item.y),
+                  fill: false,
+                  borderColor: "rgb(75, 192, 192)",
+                  tension: 0.1,
+                },
+              ],
+            }}
+            options={options}
+            ref={chartRef}
+            onClick={(e) => saveValues(e, 1)}
+          />
         </div>
-        <div
-          style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-          }}
-        >
-          <h1>Graph 2</h1>
-          <div>
-            <h3>Start Index: {selectedValues.secondGraph.startIndex}</h3>
-            <h3>End Index: {selectedValues.secondGraph.endIndex}</h3>
-          </div>
+        <div style={{ height: "50%", width: "100%",  display: "flex", justifyContent: "center"  }}>
+          <Line
+            data={{
+              labels: secondGraph.map((item) => item.x),
+              datasets: [
+                {
+                  label: "Y value",
+                  data: secondGraph.map((item) => item.y),
+                  fill: false,
+                  borderColor: "rgb(75, 0, 192)",
+                  tension: 0.1,
+                },
+              ],
+            }}
+            options={options}
+            ref={chatRef2}
+            onClick={(e) => saveValues(e, 2)}
+          />
         </div>
       </div>
-      <button onClick={(e) => onClickPoint(e)}>
-        Align Graphs'
-      </button>
-      <Line
-        data={{
-          labels: firstGraph.map((item) => item.x),
-          datasets: [
-            {
-              label: "Y value",
-              data: firstGraph.map((item) => item.y),
-              fill: false,
-              borderColor: "rgb(75, 192, 192)",
-              tension: 0.1,
-            },
-          ],
-        }}
-        options={options}
-        ref={chartRef}
-        onClick={(e) => saveValues(e, 1)}
-      />
-      <Line
-        data={{
-          labels: secondGraph.map((item) => item.x),
-          datasets: [
-            {
-              label: "Y value",
-              data: secondGraph.map((item) => item.y),
-              fill: false,
-              borderColor: "rgb(75, 0, 192)",
-              tension: 0.1,
-            },
-          ],
-        }}
-        options={options}
-        ref={chatRef2}
-        onClick={(e) => saveValues(e, 2)}
-      />
     </div>
   );
 }
+
+// draw a vertical line on cursor hover
 const options = {
   responsive: true,
   tooltips: {
     mode: "x-axis",
-    intersect: true,
+    // intersect: true,
   },
+  // interaction: {
+  //   intersect: false,
+  //   mode: "index",
+  // },
   plugins: {
     legend: {
       position: "top",
@@ -289,6 +264,11 @@ const options = {
       display: true,
       text: "Chart.js Line Chart",
     },
+    corsair: {
+        dash: [2, 2],
+        color: 'red',
+        width: 3
+      }
   },
 };
 
